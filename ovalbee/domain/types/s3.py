8e1a39cebce0584f97231f07deabd2e936b15f75
almaz <almazgimaev@gmail.com>
2025-11-03 21:02:31 +0100

    Refactor last_modified field to support both string and datetime types in S3Object model

--- ovalbee/domain/types/s3.py
+++ ovalbee/domain/types/s3.py
@@ -1,4 +1,5 @@
-from typing import Optional
+from datetime import datetime
+from typing import Optional, Union
 
 from pydantic import BaseModel, Field, field_validator
 
@@ -8,8 +9,19 @@ class S3Object(BaseModel):
 
     key: str = Field(..., description="Key of the S3 object", alias="Key")
     size: int = Field(..., description="Size of the S3 object in bytes", alias="Size")
-    last_modified: str = Field(..., description="Last modified timestamp", alias="LastModified")
+    last_modified: Union[str | datetime] = Field(
+        ..., description="Last modified timestamp", alias="LastModified"
+    )
     etag: str = Field(..., description="ETag of the S3 object", alias="ETag")
     storage_class: Optional[str] = Field(
         default=None, description="Storage class of the S3 object", alias="StorageClass"
     )
+
+    @field_validator("last_modified")
+    def validate_last_modified(cls, v):
+        if isinstance(v, str):
+            try:
+                return datetime.fromisoformat(v.replace("Z", "+00:00"))
+            except ValueError:
+                raise ValueError("last_modified must be a valid ISO 8601 datetime string")
+        return v
