c45be096cd9a4925240256278c32d641956c1729
almaz <almazgimaev@gmail.com>
2025-11-05 18:10:30 +0100

    Implement resource download functionality in AnnotationApi

--- ovalbee/api/annotation_api.py
+++ ovalbee/api/annotation_api.py
@@ -1,7 +1,9 @@
-from typing import List, Optional, Union
+import json
+import tempfile
+from typing import Any, Generator, List, Optional, Union
 
 from ovalbee.api.module_api import CRUDModuleApi
-from ovalbee.domain.types.annotation import Annotation
+from ovalbee.domain.types.annotation import Annotation, AnnotationFormat
 from ovalbee.domain.types.asset import AssetType
 from ovalbee.domain.types.base import BaseInfo
 
@@ -40,7 +42,7 @@ class AnnotationApi(CRUDModuleApi):
         return self._get_list_all_pages(
             space_id=space_id, item_type=AssetType.ANNOTATIONS.value, source_id=source_id
         )
-    
+
     def get_by_asset_id(
         self,
         space_id: int,
@@ -53,3 +55,23 @@ class AnnotationApi(CRUDModuleApi):
     # --- Update ---------------------------------------------------
     def update(self, asset_info: Annotation) -> Annotation:
         return self._update(asset_info)
+
+    # --- Download -------------------------------------------------
+    def _download_resources_generator(self, space_id: int, id: str) -> Generator[Any, None, None]:
+        annotation = self.get_info_by_id(space_id=space_id, id=id)
+        for resource in annotation.resources:
+            with tempfile.TemporaryFile(delete=True) as tmp_file:
+                bucket, key = self._api.storage._parse_s3_url(resource.url)  # TODO: implement
+                self._api.storage.download(key=key, bucket=bucket, file_path=tmp_file.name)
+                if resource.format == AnnotationFormat.YOLO:
+                    with open(tmp_file.name, "r") as f:
+                        data = f.read()
+                        yield data.splitlines()
+                elif resource.format == AnnotationFormat.SLY:
+                    with open(tmp_file.name, encoding="utf-8") as fin:
+                        yield json.load(fin)
+                else:
+                    raise ValueError(f"Unsupported annotation format: {resource.format}")
+
+    def _download_resources(self) -> List[Any]:
+        return list(self._download_resources_generator())
